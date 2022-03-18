d055959010af5ef62e17420e7e1d5500ac868917
Tyler887 <tylermageeshields@gmail.com>
2022-03-18 19:39:22 +0000

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,5 +1,6 @@
 Write-Output "Installing WINUX..."
-Invoke-WebRequest "https://github.com/Tyler887/winux/releases/download/v1.0/linux.exe" -OutFile $env:UserProfile"\AppData\Roaming\Winux\linux.exe"
+$WINUX_URL = "https://github.com/Tyler887/winux/releases/download/v1.0/linux.exe"
+Invoke-WebRequest $WINUX_URL -OutFile $env:UserProfile"\AppData\Roaming\Winux\linux.exe"
 [Environment]::SetEnvironmentVariable
      ("PATH", "$env:PATH;$env:UserProfile\AppData\Roaming\Winux", [System.EnvironmentVariableTarget]::User)
 Write-Output "WINUX has been installed! To verify, run: linux --version yes"
