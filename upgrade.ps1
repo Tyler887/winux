0407331e42edb5a3a491ca2b5b15115820884125
Tyler887 <tylermageeshields@gmail.com>
2022-03-18 21:52:13 +0000

    Update upgrade.ps1

--- upgrade.ps1
+++ upgrade.ps1
@@ -1,2 +1,2 @@
-Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://github.com/Tyler887/winux/raw/main/uninstall.ps1')
+Invoke-Expression "powershell .\uninstall-winux.ps1"
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://github.com/Tyler887/winux/raw/main/install.ps1')
