3b911c0fe2022f868edb0b723d4fdf829a5e2cb7
Tyler887 <tylermageeshields@gmail.com>
2022-03-18 20:21:17 +0000

    Create uninstall.ps1

---
+++ uninstall.ps1
@@ -0,0 +1,32 @@
+function Remove-EnvPath {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $Path,
+
+        [ValidateSet('Machine', 'User', 'Session')]
+        [string] $Container = 'Session'
+    )
+
+    if ($Container -ne 'Session') {
+        $containerMapping = @{
+            Machine = [EnvironmentVariableTarget]::Machine
+            User = [EnvironmentVariableTarget]::User
+        }
+        $containerType = $containerMapping[$Container]
+
+        $persistedPaths = [Environment]::GetEnvironmentVariable('Path', $containerType) -split ';'
+        if ($persistedPaths -contains $Path) {
+            $persistedPaths = $persistedPaths | where { $_ -and $_ -ne $Path }
+            [Environment]::SetEnvironmentVariable('Path', $persistedPaths -join ';', $containerType)
+        }
+    }
+
+    $envPaths = $env:Path -split ';'
+    if ($envPaths -contains $Path) {
+        $envPaths = $envPaths | where { $_ -and $_ -ne $Path }
+        $env:Path = $envPaths -join ';'
+    }
+}
+Write-Output "Uninstalling WINUX..."
+Remove-EnvPath $env:UserProfile"\AppData\Roaming\Winux" "User"
+Remove-Item -Path $env:UserProfile"\AppData\Roaming\Winux" -Force
