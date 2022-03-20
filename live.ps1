f9ea48d8ee7722561a5343cd50a50220d32f5ccf
Tyler887 <tylermageeshields@gmail.com>
2022-03-20 15:40:05 +0000

    Update live.ps1

--- live.ps1
+++ live.ps1
@@ -1,3 +1,3 @@
 Invoke-WebRequest "https://raw.githubusercontent.com/Tyler887/winux/v1.0/winux.py" -OutFile $env:UserProfile"\WinuxScript.py"
-Invoke-Expression "python "$env:UserProfile"\WinuxScript.py"
+Invoke-Expression -Command "python "$env:UserProfile"\WinuxScript.py"
 Remove-Item $env:UserProfile"\WinuxScript.py"
