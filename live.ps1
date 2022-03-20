e4848f1c24e2605d18506e2ccb54a2f500ebd528
Tyler887 <tylermageeshields@gmail.com>
2022-03-20 15:45:49 +0000

    Update live.ps1

--- live.ps1
+++ live.ps1
@@ -1,3 +1,7 @@
+if ( !Get-Command "python" -ErrorAction SilentlyContinue ) {
+   Write-Error "Python is required to run live." -ErrorAction Stop
+}
 Invoke-WebRequest "https://raw.githubusercontent.com/Tyler887/winux/v1.0/winux.py" -OutFile $env:UserProfile"\WinuxScript.py"
+Invoke-Expression "python -m pip install colorama questionary"
 Invoke-Expression -Command "python $env:UserProfile\WinuxScript.py"
 Remove-Item $env:UserProfile"\WinuxScript.py"
