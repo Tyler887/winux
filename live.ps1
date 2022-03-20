cd07966561b7d1298f0598ef5e4536ca03e71900
Tyler887 <tylermageeshields@gmail.com>
2022-03-20 15:48:13 +0000

    Update live.ps1

--- live.ps1
+++ live.ps1
@@ -1,5 +1,5 @@
-if ( -not Get-Command "python" -ErrorAction SilentlyContinue ) {
-   Write-Error "Python is required to run live." -ErrorAction Stop
+if ( -not Get-Command "python3" -ErrorAction SilentlyContinue ) {
+   Write-Error "Python 3 is required to run live." -ErrorAction Stop
 }
 Invoke-WebRequest "https://raw.githubusercontent.com/Tyler887/winux/v1.0/winux.py" -OutFile $env:UserProfile"\WinuxScript.py"
 Invoke-Expression "python -m pip install colorama questionary"
