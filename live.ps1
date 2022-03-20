bed01befaac633a06ab88c3628a55365a46fbd96
Tyler887 <tylermageeshields@gmail.com>
2022-03-20 16:10:57 +0000

    Update live.ps1

--- live.ps1
+++ live.ps1
@@ -3,5 +3,5 @@ if ( Get-Command "python3" -ErrorAction SilentlyContinue ) {} else {
 }
 Invoke-WebRequest "https://raw.githubusercontent.com/Tyler887/winux/v1.0/winux.py" -OutFile $env:UserProfile"\WinuxScript.py"
 Invoke-Expression "python -m pip install colorama questionary"
-Invoke-Expression -Command "start python $env:UserProfile\WinuxScript.py"
+Invoke-Expression -Command "start cmd /C python $env:UserProfile\WinuxScript.py"
 Remove-Item $env:UserProfile"\WinuxScript.py"
