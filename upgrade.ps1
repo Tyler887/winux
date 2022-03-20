058a86164c19eb8a5dfc225e8230ff9819f86b48
Tyler887 <tylermageeshields@gmail.com>
2022-03-20 20:16:02 +0000

    Update upgrade.ps1

--- upgrade.ps1
+++ upgrade.ps1
@@ -1,2 +1,41 @@
-Invoke-Expression "powershell .\uninstall-winux.ps1"
+$WinuxIsRunning = Get-Process linux -ErrorAction SilentlyContinue
+$WinuxStubIsRunning = Get-Process winux -ErrorAction SilentlyContinue
+
+function Remove-EnvPath {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $Path,
+
+        [ValidateSet('Machine', 'User', 'Session')]
+        [string] $Container = 'Session'
+    )
+
+    if ($Container -ne 'Session') {
+        $containerMapping = @{
+            Machine = [EnvironmentVariableTarget]::Machine
+            User = [EnvironmentVariableTarget]::User
+        }
+        $containerType = $containerMapping[$Container]
+
+        $persistedPaths = [Environment]::GetEnvironmentVariable('Path', $containerType) -split ';'
+        if ($persistedPaths -contains $Path) {
+            $persistedPaths = $persistedPaths | where { $_ -and $_ -ne $Path }
+            [Environment]::SetEnvironmentVariable('Path', $persistedPaths -join ';', $containerType)
+        }
+    }
+
+    $envPaths = $env:Path -split ';'
+    if ($envPaths -contains $Path) {
+        $envPaths = $envPaths | where { $_ -and $_ -ne $Path }
+        $env:Path = $envPaths -join ';'
+    }
+}
+Write-Output "Stopping all WINUX shells to prevent an error..."
+if ( $WinuxIsRunning ) { Stop-Process -Name "linux" }
+if ( $WinuxStubIsRunning ) { Stop-Process -Name "winux" }
+Write-Output "Uninstalling WINUX..."
+Remove-EnvPath $env:UserProfile"\AppData\Roaming\Winux" "User"
+Remove-Item -Path $env:UserProfile"\AppData\Roaming\Winux" -Force -Recurse
+Remove-Item -Path $env:UserProfile"\update-winux.ps1" -Force
+Remove-Item -Path $env:UserProfile"\uninstall-winux.ps1" -Force
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://github.com/Tyler887/winux/raw/main/install.ps1')
