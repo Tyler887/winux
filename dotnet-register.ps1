264cbc13cf1df53b961fd2a0af73529c416b5bce
Tyler887 <tylermageeshields@gmail.com>
2022-04-04 21:54:04 +0100

    Create dotnet-register.ps1

---
+++ dotnet-register.ps1
@@ -0,0 +1,21 @@
+if { Test-Path "$env:userprofile\AppData\Roaming\Winux" } {} else {
+  Write-Host "Error:" -NoNewline -f red
+  Write-Host " You need to install Winux before running this .NET registration script."
+  return
+}
+#  show .NET SDK logo
+Write-Host "      " -NoNewline
+Write-Host "['''''''']" -b magenta
+Write-Host "      " -NoNewline
+Write-Host "[  .NET  ]" -b magenta
+Write-Host "      " -NoNewline
+Write-Host "[.....sdk]" -b magenta
+Write-Host "  Winux Registration"
+Write-Host "-----------------------"
+Write-Host "This script registers Winux as a .NET app so you can run it simply by typing:"
+Write-Host "                                                           " -b gray
+Write-Host "   dotnet winux                                            " -b gray -f yellow
+Write-Host "                                                           " -b gray
+Write-Host "Copying Winux to directory..."
+Copy-Item "$env:userprofile\AppData\Roaming\Winux\linux.exe" -Destination "$env:userprofile\AppData\Roaming\Winux\dotnet-winux.exe"
+Write-Host "Done!"
